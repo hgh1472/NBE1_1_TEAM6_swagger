d8750106cef7489239d82056cf89c8cf8016f60b
hgh1472 <124065095+hgh1472@users.noreply.github.com>
2024-09-12 16:14:06 +0900

    Update swagger-initializer.js

--- coffee/swagger-initializer.js
+++ coffee/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://raw.githubusercontent.com/hgh1472/NBE1_1_TEAM6_swagger/main/coffee/swagger/swagger.json",
+    url: "https://raw.githubusercontent.com/hgh1472/NBE1_1_TEAM6_swagger/main/coffee/swagger.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
